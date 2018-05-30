cdf5b96d3f23ccb00228b30dc545e322a9091746
Joao Paulo Santos <jpsantos@gmail.com>
2018-05-30 18:32:48 -0300

    more coverage

--- test/SecureRandon.test.js
+++ test/SecureRandon.test.js
@@ -2,16 +2,8 @@ import SecureRandon from '../src/lib/SecureRandom';
 
 describe('Secure Randon Tests', () => {
   test('word', () => {
-    let random = SecureRandon.wordWith(16);
+    const random = SecureRandon.wordWith(16);
     expect(random).toBeDefined();
     expect(random).toHaveLength(16);
-
-    random = SecureRandon.wordWith(64);
-    expect(random).toBeDefined();
-    expect(random).toHaveLength(64);
-
-    random = SecureRandon.wordWith(8);
-    expect(random).toBeDefined();
-    expect(random).toHaveLength(8);
   });
 });
