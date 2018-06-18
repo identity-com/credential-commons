431906ecb8d29ba499d1e4c424d414cf5b87432a
Daniel Santos <danielmelos@gmail.com>
2018-06-18 15:36:23 -0300

    Improving the distribution of the package to github tags and npm

--- dist/browser/logger.js
+++ dist/browser/logger.js
@@ -1,17 +1,1 @@
-'use strict';
-
-var civicLog = require('civic-log');
-
-var logger = civicLog({
-  loggly: {
-    level: 'debug',
-    subdomain: 'civicteam',
-    tags: []
-  },
-  dstream: {
-    env: 'dev',
-    region: 'us-east-1'
-  }
-});
-
-module.exports = logger;
+"use strict";var civicLog=require("civic-log"),logger=civicLog({loggly:{level:"debug",subdomain:"civicteam",tags:[]},dstream:{env:"dev",region:"us-east-1"}});module.exports=logger;
--- dist/minified/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var UCA=require("./uca/UserCollectableAttribute"),VC=require("./creds/VerifiableCredential"),_require=require("./services/index"),initServices=_require.initServices;function CredentialCommons(a,b){return a&&initServices(a),b&&initServices(b),this.UCA=UCA,this.VC=VC,this}module.exports=new CredentialCommons;
