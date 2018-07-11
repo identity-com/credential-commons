cbc4509e5329d26164893ae4a8159c2e712a5ee2
dmelosantos <danielmelos@gmail.com>
2018-07-11 12:31:43 -0300

    CCS-155 Commenting the code more

--- src/services/ChainAuthAnchorServiceImpl.js
+++ src/services/ChainAuthAnchorServiceImpl.js
@@ -25,6 +25,13 @@ function CurrentCivicAnchor(config) {
     throw new Error('Not implemented');
   };
 
+  /**
+   * Returns an offline attestation locally for fast verification of signing
+   * @param label the transaction label
+   * @param data the merkle root from our VC
+   * @param options keychains prv, the wallet passphrase, the network to spend the funds
+   * @returns {Promise<void>} an temp attestation (signature.anchor with type="temporary")
+   */
   this.anchor = async (label, data, options) => {
     const opts = options || {};
     const keychain = opts.keychain || this.config.keychain;
@@ -49,6 +56,12 @@ function CurrentCivicAnchor(config) {
     return attester.tempAttest(request);
   };
 
+  /**
+   * Call this method to effectively put the transaction on the chain
+   * It can take up to 11 seconds (currently with BitGo implementation) to return the confirmation that is on the chain
+   * @param tempAnchor the signature.anchor with type="temporary"
+   * @returns {Promise<*>} returns the VC with the newly signed anchoring and Merkle Root on the chain
+   */
   this.update = async (tempAnchor) => {
     if (tempAnchor.type === 'temporary') {
       const attester = new Attester({
@@ -95,6 +108,7 @@ function CurrentCivicAnchor(config) {
    * This method checks that the attestation / anchor exists on the BC
    */
   this.verifyAttestation = async (signature) => {
+    let validation = false;
     if (signature.anchor.type === 'permanent' && !signature.anchor.subject.value) {
       const bitgoEnv = signature.anchor.network === 'testnet' ? 'test' : 'prod';
       const verifier = new Verifier({
@@ -103,21 +117,38 @@ function CurrentCivicAnchor(config) {
         accessToken: process.env.ACCESS_TOKEN,
         offline: false,
       });
-      const validation = await verifier.verify(signature.anchor);
-      return validation;
+      validation = await verifier.verify(signature.anchor);
     }
-    return false;
+    return validation;
   };
 
+  /**
+   * Revoke the attestation by spending it's funds
+   * @param signature the VC signature part (with the anchor)
+   * @returns {Promise<*>} an promise that verifies the funds on the chain confirming the revocation
+   */
   this.revokeAttestation = async (signature) => {
+    // TODO we have to get civic keychain from an endpoint
     const keychains = [{ prv: process.env.CIVIC_KEYCHAIN }];
     const wallet = await this.getWalletHandle(signature.anchor, process.env.CLIENT_ACCESS_TOKEN);
     await this.revokeAttestationWithWalletAndCosigner(wallet, signature.anchor, keychains);
     return this.verifyFundsSpent(signature.anchor);
   };
 
+  /**
+   * Check that an VC is revoked by going to chain and checking if the funds of the UTXO were spent
+   * @param signature
+   * @returns {Promise<*>}
+   */
   this.isRevoked = async signature => this.verifyFundsSpent(signature.anchor);
 
+  /**
+   * Method to check the funds of an UTXO. Retries 10 times by default, if no unspent found than it will fail
+   * If unspent is found check the funds
+   * @param attestation the attestation to check signature.anchor from VC
+   * @param retries how many retries should the method do, 10 by default
+   * @returns {Promise<T>} true | false if the funds were spent or not
+   */
   this.verifyFundsSpent = async (attestation, retries = 10) => {
     // Verify that the funds at the address are spent, effectively revoking the attestation
     // Note - the retries value passed to Verifier is 0, as we are testing the opposite of verify
@@ -144,6 +175,14 @@ function CurrentCivicAnchor(config) {
     });
   };
 
+  /**
+   * Revoke the attestation by spending the funds for the transaction, the wallet and keychains Civic|BitGo|Client are
+   * needed to sign the transaction
+   * @param wallet use getWalletHandle method to get this wallet
+   * @param attestation signature.anchor from the VC
+   * @param keychains Civic|BitGo|Client for signing the transaction
+   * @returns {Promise<boolean>} true if the transaction was sent to the BC, fails otherwise
+   */
   this.revokeAttestationWithWalletAndCosigner = async (wallet, attestation, keychains) => {
     // Prepare revocation tx
     const { revocation } = attestationRevocation({
@@ -156,6 +195,12 @@ function CurrentCivicAnchor(config) {
     return true;
   };
 
+  /**
+   * Bitgo auxiliary method to get the wallet of the client
+   * @param attestation
+   * @param accessToken
+   * @returns {Promise<*>}
+   */
   this.getWalletHandle = async (attestation, accessToken) => {
     const bitgoEnv = attestation.network === 'testnet' ? 'test' : 'prod';
     const sdk = new BitGo({ accessToken, env: bitgoEnv });
