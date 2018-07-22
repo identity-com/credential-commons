aa362870cfe1d0014b22cce0703e5e3ad3af357d
dmelosantos <danielmelos@gmail.com>
2018-07-22 10:40:37 -0300

    Sprint 4 Cleanup - Allowing UCA and Creds definitions to be visible on the exported library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const UCA = require('./uca/UserCollectableAttribute');
 const VC = require('./creds/VerifiableCredential');
+const ucaDefinitions = require('./uca/definitions');
+const credentialDefinitions = require('./creds/definitions');
 const { initServices } = require('./services/index');
 
 /**
@@ -10,6 +12,8 @@ const { initServices } = require('./services/index');
 function CredentialCommons() {
   this.UCA = UCA;
   this.VC = VC;
+  this.ucaDefinitions = ucaDefinitions;
+  this.credentialDefinitions = credentialDefinitions;
   this.init = initServices;
   return this;
 }
