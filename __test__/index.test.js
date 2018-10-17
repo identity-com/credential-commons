806be1a528b694f440865a661558a97f62ae0516
Daniel Santos <danielmelos@gmail.com>
2018-10-17 10:42:11 -0300

    Adding husky precommit hook and npm run lint to it

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,10 +1,10 @@
 const CredentialCommons = require('../src/index');
 const httpMock = require('../src/services/__mocks__/httpService');
 
+const { UCA, VC } = CredentialCommons;
+
 describe('Module Entry Point Tests', () => {
   it('should access the entry point e see if the modules are declared', () => {
-    const UCA = CredentialCommons.UCA;
-    const VC = CredentialCommons.VC;
     const confMock = {
       sipSecurityService: '',
       attestationService: '',
