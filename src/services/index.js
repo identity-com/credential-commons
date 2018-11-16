b086f18b0cfbcbc8b21ee860bc7dca026020cd60
Daniel Santos <danielmelos@gmail.com>
2018-11-16 11:33:30 -0200

    Fixed an merge problem

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@
  */
 const Bottle = require('bottlejs');
 const { CurrentCivicAnchor } = require('./DummyAnchorServiceImpl.js');
+const AnchorService = require('./anchorService');
 const logger = require('../logger');
 const HttpServiceConstructor = require('./httpService');
 const config = require('./config');
@@ -42,6 +43,13 @@ services.service('Http', HttpServiceConstructor);
 
 services.service('SecureRandom', SecureRandom);
 
-services.service('AnchorService', CurrentCivicAnchor, 'Config', 'Http');
+services.service('CivicAnchor', CurrentCivicAnchor, 'Config', 'Http');
+
+services.factory('AnchorService', (container) => {
+  // Here we can execute logic to replace the implementation
+  const civicAnchor = container.CivicAnchor;
+  logger.debug('Registering AnchorService with Civic implementation');
+  return new AnchorService(civicAnchor);
+});
 
 module.exports = { services, initServices };
