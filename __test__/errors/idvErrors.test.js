76fa9fccff1e62d434664042b6b53bb7f4e8a497
christiansaiki <christian@revmob.com>
2019-03-26 15:55:21 -0300

    added test to check IDV Errors

---
+++ __test__/errors/idvErrors.test.js
@@ -0,0 +1,7 @@
+const { IDVErrorCodes } = require('../../src/errors/idvErrors');
+
+describe('IDV Errors', () => {
+  it('Should have IDV Error Codes', () => {
+    expect(IDVErrorCodes).toHaveProperty('ERROR_IDV_UCA_MISSING_PROPERTY');
+  });
+});
