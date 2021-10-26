0dad5e41d1d7447d862f2ee965af6240504c95c9
William Brooks <william@identity.org>
2021-10-26 19:15:24 +0200

    Temporarily put in a noop logger

--- src/logger.js
+++ src/logger.js
@@ -1,18 +1,31 @@
-const BrowserConsole = require('winston-transport-browserconsole').default;
+// const BrowserConsole = require('winston-transport-browserconsole').default;
+//
+// const { createLogger, format } = require('winston');
+//
+// // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
+// const logger = createLogger({
+//   // To see more detailed errors, change this to 'debug'
+//   level: 'info',
+//   format: format.combine(
+//     format.splat(),
+//     format.simple(),
+//   ),
+//   transports: [
+//     new BrowserConsole(),
+//   ],
+// });
 
-const { createLogger, format } = require('winston');
-
-// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
-const logger = createLogger({
-  // To see more detailed errors, change this to 'debug'
-  level: 'info',
-  format: format.combine(
-    format.splat(),
-    format.simple(),
-  ),
-  transports: [
-    new BrowserConsole(),
-  ],
-});
-
-module.exports = logger;
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  warn(a, b = undefined) {
+  },
+  // eslint-disable-next-line no-unused-vars
+  info(a, b = undefined) {
+  },
+  // eslint-disable-next-line no-unused-vars
+  debug(a, b = undefined) {
+  },
+  // eslint-disable-next-line no-unused-vars
+  error(a, b = undefined) {
+  },
+};
