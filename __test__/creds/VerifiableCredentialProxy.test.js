43018c3ca96de0e313379786e46d86f39a761e06
William Brooks <william@identity.org>
2022-01-21 09:33:29 +0200

    IDCOM-1427 Removed unnescessary code change

--- __test__/creds/VerifiableCredentialProxy.test.js
+++ __test__/creds/VerifiableCredentialProxy.test.js
@@ -1863,7 +1863,7 @@ describe('Signned Verifiable Credentials', () => {
       new CredentialSignerVerifier({ prvBase58 }));
     expect(cred).toBeDefined();
     expect(cred.proof.merkleRootSignature).toBeDefined();
-    expect(cred.verifyMerkletreeSignature(pubBase58)).toBe(true);
+    expect(cred.verifyMerkletreeSignature(pubBase58)).toBeTruthy();
   });
 
   test('Should verify credential(data only) signature', async () => {
