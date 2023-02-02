75d64f7d5e67bab67e986f269130217a3790d33b
William Brooks <william@identity.org>
2023-02-02 12:19:05 +0200

    Replaced logging with console.log instead

--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,22 @@
-const { createLogger, format, transports } = require('winston');
-
-// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
-const logger = createLogger({
-  // To see more detailed errors, change this to 'debug'
-  level: 'info',
-  format: format.combine(
-    format.splat(),
-    format.simple(),
-  ),
-  transports: [
-    new transports.Console(),
-  ],
-});
-
-module.exports = logger;
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  warn(a, b = undefined) {
+    // eslint-disable-next-line no-console
+    console.warn(a);
+  },
+  // eslint-disable-next-line no-unused-vars
+  info(a, b = undefined) {
+    // eslint-disable-next-line no-console
+    console.info(a);
+  },
+  // eslint-disable-next-line no-unused-vars
+  debug(a, b = undefined) {
+    // eslint-disable-next-line no-console
+    // console.debug(a); // disabled
+  },
+  // eslint-disable-next-line no-unused-vars
+  error(a, b = undefined) {
+    // eslint-disable-next-line no-console
+    console.error(a);
+  },
+};
