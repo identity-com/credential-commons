38a7692065e64f1e145fae2e1900d88ddeb07b86
William Brooks <william@identity.org>
2023-02-10 11:48:29 +0200

    Removed dist/ folder
    The dist folder is generated at build time and not needed as part of
    the repository.
    
    PR: https://github.com/identity-com/credential-commons/pull/207
    
    Signed-off-by: William Brooks <william@identity.org>

--- dist/browser/constants/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var headers=require("./headers");module.exports={headers:headers};
--- dist/browser/errors/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var idvErrors=require("./idvErrors"),_require=require("./definitions"),ErrorCodes=_require.ErrorCodes,ErrorContextTypes=_require.ErrorContextTypes;module.exports={ErrorCodes:ErrorCodes,ErrorContextTypes:ErrorContextTypes,idvErrors:idvErrors};
