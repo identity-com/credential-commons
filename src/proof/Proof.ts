71ddf303336602c312278804b53b003a3d331632
william-brooks <william@identity.org>
2023-04-17 17:06:43 +0200

    Revert "Separate VC creation from the proof and add JSON-LD proof support"

--- src/proof/Proof.ts
+++
@@ -1,7 +0,0 @@
-import {VerifiableCredential} from "../vc/VerifiableCredential";
-
-export default interface Proof<K> {
-    sign(credential: VerifiableCredential, key: K): Promise<VerifiableCredential>;
-
-    verify(credential: VerifiableCredential): Promise<boolean>
-}
